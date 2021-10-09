63e6c1fcbfd4390b6153606f9636550a0a1c6d8c
Olukemi Odujinrin <47333291+Olukemi@users.noreply.github.com>
2021-10-09 12:40:32 -0400

    Add files via upload

---
+++ scripts/objects.js
@@ -0,0 +1,42 @@
+// --- objects --- //
+class Objects {
+	constructor() {
+	}
+	spaceship(x, y, spaceship_image) {
+		this.x = x;
+		this.y = y;
+		this.width = 34;
+		this.height = 28;
+		this.spaceship_image = spaceship_image;
+		this.spaceship_image.src = "images/ship.png";
+		return this;
+	}
+	bullet(x, y) {
+		this.x = x;
+		this.y = y;
+		return this;
+	}
+	ufo(x, y, line, column, ufo_image, level) {
+		this.x = x;
+		this.y = y;
+		this.line = line;
+		this.column = column;
+		this.width = 32;
+		this.height = 24;
+		this.ufo_image = ufo_image;
+		this.level = level;
+		//even-odd level selector		 
+		this.ufo_image.src = (this.level % 2 == 0) ? "images/ufo2.png" : "images/ufo.png";
+		return this;
+	}
+	bomb(x, y) {
+		this.x = x;
+		this.y = y;
+		return this;
+	}
+}
+;
+
+
+
+
---
+++ scripts/sounds.js
@@ -0,0 +1,48 @@
+// --- sounds --- //
+class Sounds {
+    constructor() {
+        this.muted = false;
+    }
+    init() {
+        this.soundsSource = ["sounds/shot.mp3", "sounds/ufoDeath.mp3", "sounds/explosion.mp3"];
+        this.allSounds = [];
+        for (let i = 0; i < this.soundsSource.length; i++) {
+            this.allSounds[i] = new Audio();
+            this.allSounds[i].src = this.soundsSource[i];
+            this.allSounds[i].setAttribute("preload", "auto");
+        }
+    }
+    playSound(soundName) {
+        if (this.muted === true) {
+            return;
+        }
+        let soundNumber;
+        switch (soundName) {
+            case 'shot':
+                soundNumber = 0;
+                break;
+            case 'ufoDeath':
+                soundNumber = 1;
+                break;
+            case 'explosion':
+                soundNumber = 2;
+                break;
+            default:
+                break;
+        }
+        this.allSounds[soundNumber].play();
+        this.allSounds[soundNumber].currentTime = 0;
+    }
+    muteSwitch() {
+        if (this.muted === false) {
+            this.muted = true;
+        }
+        else if (this.muted === true) {
+            this.muted = false;
+        }
+    }
+}
+
+
+
+
